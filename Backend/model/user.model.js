8f6f2228a220f574e1ff1d3f301739f06bdcfc15
Dhiraj Kene <kdprakash1712@gmail.com>
2026-03-29 22:49:38 +0530

    change in user model

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     Avatar:{
         type:String,
+        mrequired:true,
         unique:true
     },
     FirstName:{
