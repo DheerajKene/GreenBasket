ef473c52767f37d314bcb3056e033864e2665a93
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-03 22:48:21 +0530

    change in main file

--- Frontend/Frontend/src/main.jsx
+++ Frontend/Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import{Routes, Route} from 'react-router-dom'
-import Splash from './componants/Splash/Splash.jsx'
+// import Splash from './componants/Splash/Splash.jsx'
 import App from './App.jsx'
 import { AuthContextProvider } from './Context/AuthContext.jsx'
 
