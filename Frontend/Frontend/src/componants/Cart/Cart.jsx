6d702e01bc33efba0b922cb2a38adba08a8074ef
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-14 22:54:03 +0530

    change in cart page

--- Frontend/Frontend/src/componants/Cart/Cart.jsx
+++ Frontend/Frontend/src/componants/Cart/Cart.jsx
@@ -163,7 +163,7 @@ const Cart = ({ initialItems }) => {
               <span className="price">₹{totalPrice}</span>
             </div>
             <button className="buy-all-btn" onClick={buyAll}>
-              Buy All Items
+              Buy All products
             </button>
           </div>
         </div>
